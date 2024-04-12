29a763cdd35fefe7bc5cbd1398413afc92de8d46
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2024-04-12 15:43:19 +0200

    first commit

---
+++ src/app/housing.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HousingLocation } from './housinglocation';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HousingService {
+  constructor() {}
+  getAllHousingLocations(): HousingLocation[] {
+    return this.housingLocationList;
+  }
+
+  getHousingLocationById(id: number): HousingLocation | undefined {
+    return this.housingLocationList.find(
+      (housingLocation) => housingLocation.id === id
+    );
+  }
+}
